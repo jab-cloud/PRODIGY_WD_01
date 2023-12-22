570fbb8dfe8ff36f26b09cb10e17c8987fcf8267
jab-cloud <jabesa4@gmail.com>
2023-12-22 21:01:03 +0300

    Responsive Landing page

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-window.addEventListener('scroll', function() {
-    let navMenu = document.getElementById('#navMenu');
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+ // get the navigation element
+ var nav = document.querySelector('nav');
 
-    if (scrollTop > 70) {
-        navMenu.style.backgroundColor = 'blue';
-        navMenu.style.color = 'white';
-    } else {
-        navMenu.style.backgroundColor = '#333';
-        navMenu.style.color = 'red';
-    }
-});
+ // add an event listener to the window object
+ window.addEventListener('scroll', function() {
+   // get the scroll position of the window
+   var scrollPos = window.scrollY;
+
+   // change the background color of the navigation element based on the scroll position
+   if (scrollPos > 0) {
+     // if the scroll position is greater than zero, change the background color to light blue
+     nav.style.backgroundColor = 'black';
+   } else {
+     // if the scroll position is zero, change the background color to white
+     nav.style.backgroundColor = 'white';
+   }
+ });
